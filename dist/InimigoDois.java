674642df5221630ab33dbbf52ff97d28211a536b
Kaioshow <kh.silva@hotmail.com>
2015-06-29 02:37:17 -0300

    Update InimigoDois.java

--- dist/InimigoDois.java
+++ dist/InimigoDois.java
@@ -3,7 +3,7 @@ public class InimigoDois extends Inimigo {
 	int contagem_inimigos;
 	double coordenada_x_proximo;
 	
-	public InimigoDois(int contagem_inimigos, coordenada_x_proximo){
+	public InimigoDois(int contagem_inimigos, double coordenada_x_proximo){
 		this.contagem_inimigos=contagem_inimigos;
 		this.coordenada_x_proximo=coordenada_x_proximo;
 		super(velocidade, angulo, velocidade_rotacao, next_enemy);
