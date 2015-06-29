fe391910d590479c2cdd398df9d6d44efb1457b1
Kaioshow <kh.silva@hotmail.com>
2015-06-29 02:00:17 -0300

    Update Player.java

--- dist/Player.java
+++ dist/Player.java
@@ -2,4 +2,15 @@ public class Player extends Agente {
 	
 	double velocidade_x;
 	double velocidade_y;
+	
+	public Player(double velocidade_x, double velocidade_y){
+		this.velocidade_x=velocidade_x;
+		this.velocidade_y=velocidade_y;
+		super(explosion_start);
+		super(explosion_end);
+		super(nextShot);
+		super(raio);
+		super(coordenada_x);
+		super(coordenada_y);
+	}
 }
