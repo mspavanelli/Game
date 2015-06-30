191c3a6aa69f97bf2318c1b6b5f2a0651ef79bcd
Matheus Pavanelli <mspavanelli@gmail.com>
2015-06-29 22:34:23 -0300

    Plano de Fundo

--- dist/SegundoPlano.java
+++ dist/SegundoPlano.java
@@ -1,9 +1,10 @@
 import java.awt.Color;
 
-public class PrimeiroPlano extends Fundo {
 
-	public PrimeiroPlano(int estado, double coordenada_x, double coordenada_y,
-			double speed, double count) {
+public class SegundoPlano extends Fundo {
+
+	
+	public SegundoPlano(int estado, double coordenada_x, double coordenada_y, double speed, double count) {
 		super(estado, coordenada_x, coordenada_y, speed, count);
 	}
 
@@ -11,7 +12,7 @@ public class PrimeiroPlano extends Fundo {
 		GameLib.setColor(Color.GRAY);
 		long delta = System.currentTimeMillis() - Main.currentTime;
 		count += speed * delta;
-		GameLib.fillRect(coordenada_x, (coordenada_y + count) % GameLib.HEIGHT, 3, 3);
+		GameLib.fillRect(coordenada_x, (coordenada_y + count) % GameLib.HEIGHT, 2, 2);
 	}
 
 }
