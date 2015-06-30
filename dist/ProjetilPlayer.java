254434362ff3a61410e87b8fbd5cb5197c33f500
Kaioshow <kh.silva@hotmail.com>
2015-06-29 21:41:32 -0300

    Alteração

--- dist/ProjetilPlayer.java
+++ dist/ProjetilPlayer.java
@@ -6,7 +6,15 @@ public class ProjetilPlayer extends Projetil {
 	}
 
 	public void desenha() {
-
+		if(player_state == EXPLODING){
+				
+				double alpha = (currentTime - player_explosion_start) / (player_explosion_end - player_explosion_start);
+				GameLib.drawExplosion(player_X, player_Y, alpha);
+			}
+		else{
+			
+			GameLib.setColor(Color.BLUE);
+			GameLib.drawPlayer(player_X, player_Y, player_radius);
+		}
 	}
-
 }
