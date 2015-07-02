d7c1c818ea40f36110648355fdeb936e7d6618da
Matheus Pavanelli <mspavanelli@gmail.com>
2015-07-01 22:22:29 -0300

    Limpeza visual do código

--- dist/Main.java
+++ dist/Main.java
@@ -86,10 +86,9 @@ public class Main {
 		
 		/* estrelas que formam o fundo de primeiro plano */
 
-		// double [] background1_X = new double[20];
-		// double [] background1_Y = new double[20];
-		// double background1_speed = 0.070;
-		// double background1_count = 0.0;
+		ArrayList<PrimeiroPlano> primeiroPlano = new ArrayList<>();
+		for ( int i = 0; i < 20; i++ )
+			primeiroPlano.add( new PrimeiroPlano( 0, Math.random() * GameLib.WIDTH, Math.random() * GameLib.HEIGHT, 0.070, 0.0) );
 
 		/* estrelas que formam o fundo de segundo plano */
 
@@ -97,9 +96,6 @@ public class Main {
 		for ( int i = 0; i < 50; i++ )
 			segundoPlano.add( new SegundoPlano(0, Math.random() * GameLib.WIDTH, Math.random() * GameLib.HEIGHT, 0.045, 0.0));
 
-		ArrayList<PrimeiroPlano> primeiroPlano = new ArrayList<>();
-		for ( int i = 0; i < 20; i++ )
-			primeiroPlano.add( new PrimeiroPlano( 0, Math.random() * GameLib.WIDTH, Math.random() * GameLib.HEIGHT, 0.070, 0.0) );
 
 		/* inicializações */
 
@@ -107,18 +103,6 @@ public class Main {
 		for(int i = 0; i < inimigoProjectile.estados.length; i++) inimigoProjectile.estados[i] = INACTIVE;
 		for(int i = 0; i < inimigo1.estados.length; i++) inimigo1.estados[i] = INACTIVE;
 		for(int i = 0; i < inimigo2.estados.length; i++) inimigo2.estados[i] = INACTIVE;
-
-		// for(int i = 0; i < background1_X.length; i++){
-
-		// 	background1_X[i] = Math.random() * GameLib.WIDTH;
-		// 	background1_Y[i] = Math.random() * GameLib.HEIGHT;
-		// }
-
-		// for(int i = 0; i < background2_X.length; i++){
-
-		// 	background2_X[i] = Math.random() * GameLib.WIDTH;
-		// 	background2_Y[i] = Math.random() * GameLib.HEIGHT;
-		// }
 
 		/* iniciado interface gráfica */
 
@@ -495,14 +479,6 @@ public class Main {
 			for ( int i = 0; i < 20; i++ )
 				primeiroPlano.get(i).desenha( currentTime );
 
-			// GameLib.setColor(Color.GRAY);
-			// background1_count += background1_speed * delta;
-
-			// for(int i = 0; i < background1_X.length; i++){
-
-			// 	GameLib.fillRect(background1_X[i], (background1_Y[i] + background1_count) % GameLib.HEIGHT, 3, 3);
-			// }
-
 			/* desenhando player */
 
 			player.desenhaPlayer(currentTime);
@@ -517,7 +493,6 @@ public class Main {
 			/* desenhando inimigos (tipo 1) */
 
 			inimigo1.desenhaInimigo1(currentTime);
-
 
 			/* desenhando inimigos (tipo 2) */
 
