781e89e05adc7587b856d0a2b5a9d83416d9c03a
Lucas Marçal <lucsmarcal@gmail.com>
2015-07-03 00:48:28 -0300

    Update PowerUp1.java

--- dist/PowerUp1.java
+++ dist/PowerUp1.java
@@ -1,12 +1,33 @@
 import java.awt.Color;
+import java.util.Random;
 
-public class PowerUp1 extends PowerUp{
+public class PowerUp1 implements PowerUpState{
 	
-	public PowerUp1(int estado, double coordenada_x, double coordenada_y){
-		super(estado, coordenada_x, coordenada_y);
+	int [] estados;
+	double [] coordenada_x;
+	double [] coordenada_y;
+	double [] velocidade;
+	double [] angulo;
+	double [] raio;
+	long nextPower;
+	
+	Random random = new Random();
+	
+	public PowerUp1(int [] estados, double []coordenada_x,double []coordenada_y,double [] velocidade,double [] angulo, double []raio,long nextPower){
+		this.estados = estados;
+		this.coordenada_x = coordenada_x;
+		this.coordenada_y = coordenada_y;
+		this.velocidade = velocidade;
+		this.angulo = angulo;
+		this.raio = raio;
+		this.nextPower = nextPower;
 	}
-
-	public void desenha(){
+	
+	public void desenha(long currentTime){
 		
+		for(int i= 0; i <estados.length; i++ ){	
+			GameLib.setColor(Color.WHITE);
+			GameLib.drawSquare(coordenada_x[i], coordenada_y[i], raio[i]);
+		}
 	}
 }
