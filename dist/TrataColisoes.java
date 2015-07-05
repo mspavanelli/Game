cc67c7fb10338bfb3a9584fce5bb23060aebfa18
Matheus Pavanelli <mspavanelli@gmail.com>
2015-07-05 01:31:27 -0300

    Classe para o tratamento de colisões
    Incompleta

--- dist/TrataColisoes.java
+++ dist/TrataColisoes.java
@@ -2,6 +2,17 @@
 
 public class TrataColisoes {
 	
-	/* Sequeência de métodos estáticos */
-	
+	private Player player = null;
+	private Inimigo inimigos1 = null;
+	private Inimigo inimigos2 = null;
+	private Inimigo inimigos3 = null;
+
+	public TrataColisoes( Player p, Inimigo i1, Inimigo i2, Inimigo i3 ) {
+		player = p;
+		inimigos1 = i1;
+		inimigos2 = i2;
+		inimigos3 = i3;
+	}
+
+
 }
