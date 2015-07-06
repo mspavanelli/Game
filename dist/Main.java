ec4160ed7eb88b4bd16ac3151648077e90b0a458
gjmb <gjmbarbosa@gmail.com>
2015-07-05 21:29:31 -0300

    Update Main.java

--- dist/Main.java
+++ dist/Main.java
@@ -3,11 +3,6 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Main {
-
-	public static final int INACTIVE = 0;
-	public static final int ACTIVE = 1;
-	public static final int EXPLODING = 2;
-
 	public static void busyWait(long time) {
 
 		while (System.currentTimeMillis() < time)
@@ -47,33 +42,32 @@ public class Main {
 
 		long delta;
 		long currentTime = System.currentTimeMillis();
-
+		//Instancição do player
 		Player player = new Player(new PowerUp2(false, 0, Math.random() * (GameLib.WIDTH - 20.0) + 10.0, -10.0, 0.07 + Math.random() * 0.07, 3 * Math.PI / 2, 0.0, 9.0, currentTime + 25000),
 		new	PlayerProjectile(new int[10], new double[10], new double[10], new double[10], new double[10], 	new PowerUp1(false, 0, Math.random() * (GameLib.WIDTH - 20.0) + 10.0, -10.0, 0.07 + Math.random() * 0.07,
-		 3 * Math.PI / 2, 0.0, 9.0, currentTime + 20000)),ACTIVE, GameLib.WIDTH / 2, GameLib.HEIGHT * 0.90, 0.25, 0.25, 12.0, 0, 0, currentTime);
-
+		 3 * Math.PI / 2, 0.0, 9.0, currentTime + 20000)),1, GameLib.WIDTH / 2, GameLib.HEIGHT * 0.90, 0.25, 0.25, 12.0, 0, 0, currentTime);
+		//Instancição da lista de inimigos do tipo1
 		ListInimigo1 listaInimigo1 = new ListInimigo1(new LinkedList<Inimigo1>(), currentTime + 2000);
-
+		//Instanciação da lista(unitária) de inimigos do tipo 2
 		ListInimigo2 listaInimigo2 =new ListInimigo2(new LinkedList<Inimigo2>());
+		//Adição de um inimigo do tipo 2 na lista
 		listaInimigo2.addLista(currentTime,1);
-
+		//Instanciação da lista(unitária) de inimigos do tipo 3
 		ListInimigo3 listaInimigo3 = new ListInimigo3(new LinkedList<Inimigo3>());
-
+		//Adição de um inimigo do tipo 3 na lista
 		listaInimigo3.addLista(currentTime,1);
 
 		// variáveis dos projéteis lançados pelos inimigos (tanto tipo 1, quanto * tipo 2, quanto tipo 3)
-
 		InimigoProjectile inimigoProjectile = new InimigoProjectile(new int[200], new double[200], new double[200], new double[200], new double[200], 2.0);
 
 		/* Plano de Fundo (próximo e distante) */
 		ArrayList<PrimeiroPlano> primeiroPlano = new ArrayList<>();
 		for (int i = 0; i < 20; i++)
 			primeiroPlano.add(new PrimeiroPlano(0, Math.random() * GameLib.WIDTH, Math.random() * GameLib.HEIGHT, 0.070, 0.0));
-
 		ArrayList<SegundoPlano> segundoPlano = new ArrayList<>();
 		for (int i = 0; i < 50; i++)
 			segundoPlano.add(new SegundoPlano(0, Math.random() * GameLib.WIDTH, Math.random() * GameLib.HEIGHT, 0.045, 0.0));
-
+		//Instanciação da Interface do Jogo
 		GameFacade facade=new GameFacade(listaInimigo1, listaInimigo2, listaInimigo3, player, inimigoProjectile, primeiroPlano, segundoPlano);
 
 		/* iniciado interface gráfica */
@@ -103,13 +97,13 @@ public class Main {
 
 			delta = System.currentTimeMillis() - currentTime;
 			currentTime = System.currentTimeMillis();
-
+			//chamada de metodo da interface que verifica as colisões
 			facade.verificaColisoes(currentTime);
-
+			//chamada do método que atualiza os estados dos elementos do jogo
 			facade.atualizaEstado(currentTime, delta);
-
+			//chamada do método que permite o controle do player
 			running = facade.verificaControlePlayer( running,  currentTime, delta);
-
+			//chamada do método que desenha a cena
 			facade.desenhaCena(currentTime, delta);
 
 			GameLib.display();
